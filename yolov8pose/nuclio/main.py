85d6b25fd81b92465aefe8c2835dab05e669afe4
Naeem Iqbal <naeem.iqbal@dfki.de>
2025-01-06 13:22:12 +0100

    Added both crop and weed classes with svg skeleton

--- yolov8pose/nuclio/main.py
+++ yolov8pose/nuclio/main.py
@@ -18,7 +18,7 @@ except RuntimeError as e:
     pass  # Continue execution without debugger
 
 iou_threshold = 0.2
-conf_threshold = 0.6
+conf_threshold = 0.7
 # "labels": {0: 'soil', 1: 'crop', 2: 'weed'}
 def init_context(context):
     context.logger.info("Init detector...")
@@ -49,34 +49,34 @@ def handler(context, event):
     for instance_idx in range(pred_instances.boxes.shape[0]):
         box, cls, conf = pred_instances.boxes.xyxy[instance_idx, :], pred_instances.boxes.cls[instance_idx].item(), pred_instances.boxes.conf[instance_idx].item()
         keypoints, keypoint_scores = pred_instances.keypoints.xy[instance_idx, 0, :], pred_instances.keypoints.conf[instance_idx].item()
-        for label in context.user_data.labels:
-            elements = []
-            for element in label["sublabels"]:
-                if element["name"] == "center":
-                    points = [float(keypoints[0]),float(keypoints[1])]
-                elif element["name"] == "top_left":
-                    points = [float(box[0].item()),float(box[1].item())]
-                elif element["name"] == "top_right":
-                    points = [float(box[2].item()),float(box[1].item())]
-                elif element["name"] == "bottom_left":
-                    points = [float(box[0].item()),float(box[3].item())]
-                elif element["name"] == "bottom_right":
-                    points = [float(box[2].item()),float(box[3].item())]
-                elements.append({
-                    "label": element["name"],
-                    "type": "points",
-                    "outside": 0 if conf_threshold < keypoint_scores else 1,
-                    "points": points,
-                    "confidence": str(keypoint_scores),
-                })
-            skeleton = {
-                "confidence": str(conf),
-                "label": label["name"],
-                "type": "skeleton",
-                "elements": elements
-            }
+        label = context.user_data.labels[int(cls)-1] # The class indices are 1-indexed since no background. 
+        elements = []
+        for element in label["sublabels"]:
+            if element["name"] == "center":
+                points = [float(keypoints[0]),float(keypoints[1])]
+            elif element["name"] == "top_left":
+                points = [float(box[0].item()),float(box[1].item())]
+            elif element["name"] == "top_right":
+                points = [float(box[2].item()),float(box[1].item())]
+            elif element["name"] == "bottom_left":
+                points = [float(box[0].item()),float(box[3].item())]
+            elif element["name"] == "bottom_right":
+                points = [float(box[2].item()),float(box[3].item())]
+            elements.append({
+                "label": element["name"],
+                "type": "points",
+                "outside": 0 if conf_threshold < keypoint_scores else 1,
+                "points": points,
+                "confidence": str(keypoint_scores),
+            })
+        skeleton = {
+            "confidence": str(conf),
+            "label": label["name"],
+            "type": "skeleton",
+            "elements": elements
+        }
 
-            if not all([element['outside'] for element in skeleton["elements"]]):
-                results.append(skeleton)
+        if not all([element['outside'] for element in skeleton["elements"]]):
+            results.append(skeleton)
 
     return context.Response(body=json.dumps(results), headers={}, content_type="application/json", status_code=200)
